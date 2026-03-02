e4d3a04779ec07f3cbf9850e2864e1c2a79d6811
finalSay9 <chimwazaevan1@gmail.com>
2026-03-02 18:05:51 +0200

    hello

---
+++ app/datascience/notebooks/train_model.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+from fastapi import APIRouter
+import os
+
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(BASE_DIR, "house.csv")
+
+
+
+df = pd.read_csv(file_path)
+df["Month"] = pd.to_datetime(df["Month"]).dt.month
+df = pd.get_dummies(df, columns=["District"])
+df.describe()
+df.info()
+df['District'].value_counts()
+
+
+print(df)
+
+
+
+
+
+print(df.head())
